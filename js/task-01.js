daf8719c69287a41332253590c280fdecbba711b
Roman <romeenice@gmail.com>
2022-10-24 21:22:33 -0700

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,5 @@
+const categories = document.querySelectorAll(".item");
+const category = document.querySelectorAll("");
+
+console.log("Number of categories: " + categories.length);
+console.log(category);
