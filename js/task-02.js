2213471e8801b8587418356e785aacfad13599b1
Roman <romeenice@gmail.com>
2022-10-26 20:29:23 -0700

    1-4

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const storageEll = [];
+const listEl = document.querySelector("#ingredients");
+ingredients.forEach((ingredient) => {
+  const itemEL = document.createElement("li");
+  itemEL.classList.add("item");
+  itemEL.textContent = ingredient;
+  storageEll.push(itemEL);
+});
+listEl.append(...storageEll);
