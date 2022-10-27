100ef75654258d4ad4db1b185fb1e93dbde6a902
Roman <romeenice@gmail.com>
2022-10-26 21:11:33 -0700

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,8 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+createBtn = document.querySelector("[daa-create]");
+destroyBtn = document.querySelector("[data-destroy");
+input = document.querySelector("input");
+box = document.querySelector("boxes");
