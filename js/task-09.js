1bf9b96315b35fb8a756754ceb17fcc758049d69
Roman <romeenice@gmail.com>
2022-10-26 21:04:21 -0700

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+const body = document.body;
+const btnColor = body.querySelector(".change-color");
+const spanEL = body.querySelector(".color");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+btnColor.addEventListener("click", onClickBtnChangeColor);
+
+function onClickBtnChangeColor(event) {
+  body.style.backgroundColor = getRandomHexColor();
+  spanEL.textContent = body.style.backgroundColor;
 }
