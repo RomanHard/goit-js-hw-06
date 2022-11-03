94815ce5e114af9a5f77a24d49ff18f1f0473f45
Roman <romeenice@gmail.com>
2022-11-03 10:10:37 -0700

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,26 +1,26 @@
-// const inputEL = document.querySelector("#validation-input");
-// const chekOn = (event) => {
-//   event.currentTarget.classList.remove("invalid");
-//   if (
-//     event.currentTarget.value.trim().length ===
-//     event.currentTarget.dataset.length
-//   ) {
-//     event.currentTarget.classList.add("invalid");
+// =============================
+// const inputElm = document.querySelector("#validation-input");
+// inputElm.addEventListener("blur", onBlur);
+// function onBlur(event) {
+//   const input = event.currentTarget;
+//   const inputLength = Number(input.dataset.length);
+//   if (input.value.length === inputLength) {
+//     input.classList.remove("invalid");
+//     input.classList.add("valid");
+//   } else {
+//     input.classList.remove("valid");
+//     input.classList.add("invalid");
 //   }
-//   event.currentTarget.classList.add("valid");
-// };
-// inputEL.addEventListener("blur", chekOn);
+// }
 
-const inputElm = document.querySelector("#validation-input");
-inputElm.addEventListener("blur", onBlur);
-function onBlur(event) {
-  const input = event.currentTarget;
-  const inputLength = Number(input.dataset.length);
-  if (input.value.length === inputLength) {
-    input.classList.remove("invalid");
-    input.classList.add("valid");
-  } else {
-    input.classList.remove("valid");
-    input.classList.add("invalid");
+const inputEl = document.querySelector("#validation-input");
+const onCheckInputContent = (event) => {
+  event.currentTarget.classList.remove("invalid");
+  if (
+    event.currentTarget.value.trim().length < event.currentTarget.dataset.length
+  ) {
+    event.currentTarget.classList.add("invalid");
   }
-}
+  event.currentTarget.classList.add("valid");
+};
+inputEl.addEventListener("blur", onCheckInputContent);
