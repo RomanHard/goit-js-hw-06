bc5178dd9475f46b255f816924f59ca8fa198ce1
Roman <romeenice@gmail.com>
2022-11-02 21:48:22 -0700

    summary dz

--- js/task-08.js
+++ js/task-08.js
@@ -6,10 +6,10 @@ function puskSubmit(event) {
   const {
     elements: { email, password },
   } = event.currentTarget;
-  if (email.value === "" || password === "") {
+  if (email.value === "" || password.value === "") {
     return alert("Всі поля повинні бути заповнені!");
   }
 
-  console.log(`Email: #{email.value}, Password: ${password.value}`);
+  console.log(`Email: ${email.value}, Password: ${password.value}`);
   event.currentTarget.reset();
 }
