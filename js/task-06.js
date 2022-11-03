a0dddd83b68e7ca5948b68e31671cb94bad63fd4
Roman <romeenice@gmail.com>
2022-11-03 10:14:13 -0700

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,18 +1,3 @@
-// =============================
-// const inputElm = document.querySelector("#validation-input");
-// inputElm.addEventListener("blur", onBlur);
-// function onBlur(event) {
-//   const input = event.currentTarget;
-//   const inputLength = Number(input.dataset.length);
-//   if (input.value.length === inputLength) {
-//     input.classList.remove("invalid");
-//     input.classList.add("valid");
-//   } else {
-//     input.classList.remove("valid");
-//     input.classList.add("invalid");
-//   }
-// }
-
 const inputEl = document.querySelector("#validation-input");
 const onCheckInputContent = (event) => {
   event.currentTarget.classList.remove("invalid");
